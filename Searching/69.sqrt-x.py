ac163edf0aac2bbf1a4307627654a4aac5f43143
faraz <syed.faraz@wavtec.com>
2025-06-28 17:15:52 +0500

    fucked up still you are calculating the wrong mid point nigga!

---
+++ Searching/69.sqrt-x.py
@@ -0,0 +1,80 @@
+#
+# @lc app=leetcode id=69 lang=python3
+#
+# [69] Sqrt(x)
+#
+# https://leetcode.com/problems/sqrtx/description/
+#
+# algorithms
+# Easy (40.37%)
+# Likes:    8947
+# Dislikes: 4588
+# Total Accepted:    2.6M
+# Total Submissions: 6.5M
+# Testcase Example:  '4'
+#
+# Given a non-negative integer x, return the square root of x rounded down to
+# the nearest integer. The returned integer should be non-negative as well.
+# 
+# You must not use any built-in exponent function or operator.
+# 
+# 
+# For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+# 
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: x = 4
+# Output: 2
+# Explanation: The square root of 4 is 2, so we return 2.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: x = 8
+# Output: 2
+# Explanation: The square root of 8 is 2.82842..., and since we round it down
+# to the nearest integer, 2 is returned.
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 0 <= x <= 2^31 - 1
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left=0
+        right=x
+        res=0
+        while left<=right:
+            mid_point=(left+right)//2
+            if mid_point**2==x:
+                res=mid_point
+            
+            elif mid_point**2<x:
+                left=mid_point+1
+
+            else:
+                right=mid_point-1
+
+        return res
+    
+
+solution=Solution()
+print(solution.mySqrt(7))
+
+            
+
+            
+        
+# @lc code=end
+
