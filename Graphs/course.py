c4a4849fb66f591dd660a3e0483b69e0c7c8edcc
faraz <syed.faraz@wavtec.com>
2025-07-22 11:13:18 +0500

    'f

--- Graphs/course.py
+++ Graphs/course.py
@@ -1,107 +1,24 @@
-numCourses = 2
-prerequisites = [[1,0]]
-def getNeighbors(G, nodes):
-    lst = []
-    for i in G[nodes]:
-        lst.append(i[0])
-    return lst
+def has_cycle(G, s):
+    stack = [s]
+    visited = [False] * len(G)
+    rec_stack = [False] * len(G)
 
-def canFinish(prerequisites,numCourses):
-    adj_list={}
+    while stack:
+        node = stack.pop()
 
+        if visited[node] == False:
+            visited[node] = True
+            rec_stack[node] = True
+            stack.append(node)
 
-    for i in range(numCourses):
-
-        adj_list[i]=[]
-
-
-    for crs,prereq in prerequisites:
-        adj_list[crs].append(prereq)
-    starting_node = 0  # or use S parameter that's passed in
-
-    def dfs(G,S):
-        i=0
-        visited_list=[]
-        stack=[]
-        stack.append(starting_node)
-
-        while len(stack)!=0:
-            visited=stack.pop()
-            neighbours=getNeighbors(G,visited)
-
-
-            for neigbour in neighbours:
-                if neigbour not in visited_list:
-                    stack.append(neigbour)
-                    break
-            visited_list.append(visited)
-            i+=1
-            # Check for cycle detection
-            visited = set()
-            rec_stack = set()
-
-            def has_cycle(node):
-                if node in rec_stack:
-                    return True
-                if node in visited:
-                    return False
-
-                visited.add(node)
-                rec_stack.add(node)
-
-                for neighbor in adj_list[node]:
-                    if has_cycle(neighbor):
+            neighbours = getNeighbors(G, node)
+            for neighbour in neighbours:
+                if visited[neighbour] == False:
+                    stack.append(neighbour)
+                else:
+                    if rec_stack[neighbour]:
                         return True
+        else:
+            rec_stack[node] = False
 
-                rec_stack.remove(node)
-                return False
-
-            for course in range(numCourses):
-                if course not in visited:
-                    if has_cycle(course):
-                        return False
-
-            return True
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-res=canFinish(prerequisites,numCourses)
-print(res)
+    return False
---
+++ Graphs/pathexits.py
@@ -0,0 +1,38 @@
+from typing import List
+
+def valid_path(n: int, edges: List[List[int]], source: int, destination: int):
+    if source == destination:
+        return True
+
+    adj_list = {}
+
+    # Initialize adjacency list
+    for i in range(n):
+        adj_list[i] = []
+
+    # Build adjacency list from edges
+    for edge in edges:
+        u, v = edge[0], edge[1]
+        adj_list[u].append(v)
+        adj_list[v].append(u)
+
+    def dfs(adj_list, starting_node):
+        stack = []
+        visited = set()
+        stack.append(starting_node)
+        while len(stack) != 0:
+            node = stack.pop()
+            if node not in visited:
+                visited.add(node)
+                if node == destination:
+                    return True
+                neighbours = adj_list[node]
+                for neighbour in neighbours:
+                    if neighbour not in visited:
+                        stack.append(neighbour)
+        return False
+
+    return dfs(adj_list, source)
+
+res = valid_path(3, [[0,1],[1,2],[2,0]], 0, 2)
+print(res)
